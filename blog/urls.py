4bdda0f00263e0686234e7f1fa44157dfb8e62bd
Mateusz <mateuszkrolik87@gmail.com>
2023-12-10 17:39:45 +0100

    🤠 Second Commit For blog app urls&views added starting_page, posts and post_detail(w/o any logic yet) Also added a slug(search engine friendly identifier) Then registered them all in project-wide urls.

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.starting_page, name="starting-page),
+    path("posts", views.posts, name="posts-page"),
+    path("posts/<slug>", views.post_detail, 
+        name="post-detail-page") #posts/my-first-post
+]

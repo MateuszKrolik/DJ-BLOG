f55ad85251f0e5b2839767a5c50e2215abb7c1a2
Mateusz <mateuszkrolik87@gmail.com>
2023-12-10 23:52:53 +0100

    🤧 6th Commit
    Created 'All Posts' page with reusable post previews.
    Added new templates for the 'All Posts' page and individual post previews.
    Refactored navigation to base template for reusability.
    Adjusted CSS files and imports for better organization and specificity.

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ def starting_page(request):
     return render(request, "blog/index.html")
 
 def posts(request):
-    pass
+    return render(request, "blog/all-posts.html")
 
 def post_detail(request):
     pass

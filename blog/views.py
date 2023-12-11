7824504a0bbfc3cdee2f5bbdbd3ed9d3a08f4966
Mateusz <mateuszkrolik87@gmail.com>
2023-12-11 15:05:58 +0100

    👀 8th Commit
    Created post-detail page with static content.
    
    Extended base HTML template and added static content including title,
    
    CSS files, and placeholder content.
    
    Added dummy paragraphs for post content.
    
    Updated views.py to render the post-detail template with a slug.

--- blog/views.py
+++ blog/views.py
@@ -8,5 +8,5 @@ def starting_page(request):
 def posts(request):
     return render(request, "blog/all-posts.html")
 
-def post_detail(request):
-    pass
+def post_detail(request, slug):
+    return render(request, "blog/post-detail.html")

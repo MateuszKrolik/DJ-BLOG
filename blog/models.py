2ae250bf293016ce487da4df30bae416d4f01670
Mateusz <mateuszkrolik87@gmail.com>
2023-12-11 20:11:48 +0100

    🐼 14th Commit
    Implemented the Author model with first_name, last_name, and email_address fields.
    Added a ForeignKey One-To-Many relation from Post to Author,
    with on_delete set to SET_NULL and related_name set to 'posts'.

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,11 @@ from django.core.validators import MinLengthValidator
 
 # Create your models here.
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email_address = models.EmailField()
+
 class Post(models.Model):
     title = models.CharField(max_length=150)
     excerpt = models.CharField(max_length=200)
@@ -10,3 +15,5 @@ class Post(models.Model):
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(
+        Author, on_delete=models.SET_NULL, related_name="posts")

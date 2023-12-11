5bb6e7077457f766a3ed22d845b7530b65b849e5
Mateusz <mateuszkrolik87@gmail.com>
2023-12-12 00:36:14 +0100

    🙈 19th Commit
    Enhanced the admin panel for posts.
    Added filters for author, tags, and date.
    Displayed title, date, and author in the posts list.
    Enabled auto-population of the slug field from the title.

--- blog/admin.py
+++ blog/admin.py
@@ -4,6 +4,11 @@ from .models import Author, Post, Tag
 
 # Register your models here.
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ("author", "tags", "date",)
+    list_display = ("title", "date", "author",)
+    prepopulated_fields = {"slug": ("title",)}
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
 admin.site.register(Tag)

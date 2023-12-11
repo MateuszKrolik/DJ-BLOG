ebbb0e93bcfb2a9440ace41bec15481de4180b09
Mateusz <mateuszkrolik87@gmail.com>
2023-12-11 19:50:49 +0100

    👑 13th Commit
    In Blog App Implemented the Post model with title, excerpt, image_name, date, slug, and content fields.
    Added unique constraint to slug and minimum length validator to content.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    excerpt = models.CharField(max_length=200)
+    image_name = models.CharField(max_length=100)  
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True, db_index=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])

edff8a82e71591438c84fec4025ccc1675c8aecf
Mateusz <mateuszkrolik87@gmail.com>
2023-12-12 00:25:28 +0100

    🦁 18th Commit
    Added new authors and tags in the admin panel.
    Overrode the str method in the Author(while also creating a full_name helper function) and Tag models for better representation.
    Created new posts and associated them with authors and tags.
    Prepared for enhancements in the admin panel for better post management.

--- blog/models.py
+++ blog/models.py
@@ -6,12 +6,20 @@ from django.core.validators import MinLengthValidator
 class Tag(models.Model):
     caption = models.CharField(max_length=20)
 
-    
+    def __str__(self):
+        return self.caption 
 
+    
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email_address = models.EmailField()
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.full_name()
 
 class Post(models.Model):
     title = models.CharField(max_length=150)

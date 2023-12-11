3a3a79a95d79753fdaa33bd9cb3f901f84393f0c
Mateusz <mateuszkrolik87@gmail.com>
2023-12-11 17:13:14 +0100

    🎃 10th Commit
    Implemented dynamic content in post.html.
    Replaced static content with dynamic data from the post dictionary.
    Updated image source, URL, and alt text to use post data.
    Also updated the post title and excerpt to use dynamic data.

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from datetime import date
 
 from django.shortcuts import render
 
-posts = [
+all_posts = [
     {
         "slug": "hike-on-the-beach",
         "image": "beach.jpg",
@@ -69,10 +69,17 @@ posts = [
 ]
 
 
+def get_date(post):
+    return post['date']
+
 # Create your views here.
 
 def starting_page(request):
-    return render(request, "blog/index.html")
+    sorted_posts = sorted(all_posts, key=get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request, "blog/index.html", {
+        "posts": latest_posts
+    })
 
 def posts(request):
     return render(request, "blog/all-posts.html")

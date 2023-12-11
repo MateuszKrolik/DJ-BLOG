939609cefde02377e1b4ce69b5be898f38cb5d89
Mateusz <mateuszkrolik87@gmail.com>
2023-12-11 17:41:26 +0100

    👺  11th Commit
    Implemented dynamic content in post-detail.html.
    Replaced static image source and alt text with dynamic data from the post dictionary.
    Updated post title, author, and date to use dynamic data.
    Applied date formatting and linebreaks filter to post content.

--- blog/views.py
+++ blog/views.py
@@ -82,7 +82,12 @@ def starting_page(request):
     })
 
 def posts(request):
-    return render(request, "blog/all-posts.html")
+    return render(request, "blog/all-posts.html", {
+        "all_posts": all_posts
+    })
 
 def post_detail(request, slug):
-    return render(request, "blog/post-detail.html")
+    indentified_post = next(post for post in all_posts if post['slug'] == slug)
+    return render(request, "blog/post-detail.html", {
+        "post": indentified_post
+    })

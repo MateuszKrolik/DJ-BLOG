553fa523de8824805cc05079eb5c3642ffa46e4a
Mateusz <mateuszkrolik87@gmail.com>
2023-12-12 23:24:09 +0100

    🔥 35th Commit
    Implemented 'Read Later' functionality.
    Added a new view 'ReadLaterView' to handle both GET and POST requests via Session.
    POST requests allow users to save post IDs to their session under 'stored_posts'.
    GET requests retrieve these IDs, fetch corresponding Post objects from the database, and pass them to 'stored-posts.html' template.
    Also, added a new URL pattern 'read-later' to urls.py to route to 'ReadLaterView'.
    As well as gave users access to those posts by adding a link to them in base.html 'main-navigation'.

--- blog/views.py
+++ blog/views.py
@@ -89,7 +89,40 @@ class SinglePostView(View):
 
 class ReadLaterView(View):
 
+    def get(self, request):
+        stored_posts = request.session.get("stored_posts")
+        
+        context = {}
+
+        if stored_posts is None or len(stored_posts) == 0:
+            context["posts"] = []
+            context["has_posts"] = False
+        else:
+            posts = Post.objects.filter(id__in=stored_posts)
+            context["posts"] = posts
+            context["has_posts"] = True
+
+        return render(request, "blog/stored-posts.html", context)
+            
+
     def post(self, request):
-        pass
+        
+        stored_posts = request.session.get("stored_posts")
+        
+        if stored_posts is None:
+            stored_posts = []
+        
+        post_id=int(request.POST["post_id"])
+
+        if post_id not in stored_posts:
+            stored_posts.append(post_id)
+            request.session["stored_posts"] = stored_posts
+        
+        return HttpResponseRedirect("/")
+    
+
+
+
+        
 
     

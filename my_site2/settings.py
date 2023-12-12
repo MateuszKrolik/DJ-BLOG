7bc434c4575e7e1e00818cc1cd8a4cb8175aaaaf
Mateusz <mateuszkrolik87@gmail.com>
2023-12-12 16:57:19 +0100

    🐴 23rd Commit
    Implemented file uploads for post images.
    Updated Post model to include an ImageField.
    Configured MEDIA_ROOT and MEDIA_URL settings.
    Installed Pillow package for image handling.
    Updated database schema with new migrations.

--- my_site2/settings.py
+++ my_site2/settings.py
@@ -129,3 +129,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_ROOT = BASE_DIR / "uploads"
+MEDIA_URL = "/files/" #called it files to make it clear that these two can differ

46981b95d962b5e2d2042e9d73598d030ef8f7ff
Mateusz <mateuszkrolik87@gmail.com>
2023-12-12 14:52:50 +0100

    🐝 20th Commit
    Updated views to fetch data from the Post model.
    Removed dummy data and implemented query to get the latest three posts.
    Fixed template to use correct field name for image.

--- blog/views.py
+++ blog/views.py
@@ -2,70 +2,10 @@ from datetime import date
 
 from django.shortcuts import render
 
+from .models import Post
+
 all_posts = [
-    {
-        "slug": "hike-on-the-beach",
-        "image": "beach.jpg",
-        "author": "Mateusz",
-        "date": date(2023,12,11),
-        "title": "Beach Hiking",
-        "excerpt": "There's nothing like the views you get when hiking on the beach! And I wasn't even prepared for what happend whilst I was enjoying the view!",
-        "content": """
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet dolorum 
-            architecto non similique veritatis voluptatum delectus aperiam earum, 
-            deserunt totam nemo ipsam! Ex explicabo corrupti sequi ratione doloribus optio ea?
-
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet dolorum 
-            architecto non similique veritatis voluptatum delectus aperiam earum, 
-            deserunt totam nemo ipsam! Ex explicabo corrupti sequi ratione doloribus optio ea?
-
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet dolorum 
-            architecto non similique veritatis voluptatum delectus aperiam earum, 
-            deserunt totam nemo ipsam! Ex explicabo corrupti sequi ratione doloribus optio ea?
-        """
- },
-    {
-        "slug": "programming-is-fun",
-        "image": "coding.jpg",
-        "author": "Mateusz",
-        "date": date(2023,12,11),
-        "title": "Programming Is Great!",
-        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "into-the-night",
-        "image": "sunset.jpg",
-        "author": "Mateusz",
-        "date": date(2023,12,11),
-        "title": "Nature At Its Best",
-        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    }
+    
 ]
 
 
@@ -75,8 +15,7 @@ def get_date(post):
 # Create your views here.
 
 def starting_page(request):
-    sorted_posts = sorted(all_posts, key=get_date)
-    latest_posts = sorted_posts[-3:]
+    latest_posts = Post.objects.all().order_by("-date")[:3]
     return render(request, "blog/index.html", {
         "posts": latest_posts
     })

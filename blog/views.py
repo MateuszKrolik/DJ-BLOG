16fc3dc577929ec584426c2d975dc8d522a106c3
Mateusz <mateuszkrolik87@gmail.com>
2023-12-12 20:52:46 +0100

    🐲 29th Commit
    Implemented form submission handling in SinglePostView.
    Added CSRF token for security.
    Configured form action to post detail page.
    Replaced DetailView with generic View and added custom get and post methods.
    In get method, fetched post, tags, and instantiated CommentForm.
    In post method, validated form, saved comment if valid, and handled redirection.
    Also handled errors while forms required field in browser is removed by user thanks to server-side validation.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views.generic import ListView
+from django.views import View
 
 from .models import Post
 from .forms import CommentForm
@@ -36,15 +39,44 @@ class AllPostsView(ListView):
 #     })
 
 
-class SinglePostView(DetailView):
-    template_name = "blog/post-detail.html"
-    model = Post
+class SinglePostView(View):
+    # template_name = "blog/post-detail.html"
+    # model = Post
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["post_tags"] = self.object.tags.all()
-        context["comment_form"] = CommentForm()
-        return context
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug) 
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm()
+        }
+        return render(request, "blog/post-detail.html", context)
+    
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post 
+            comment.save()
+
+            return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
+        
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": comment_form
+        }
+        return render(request, "blog/post-detail.html", context)
+
+
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context["post_tags"] = self.object.tags.all()
+    #     context["comment_form"] = CommentForm()
+    #     return context
 
 # def post_detail(request, slug):
 #     indentified_post = get_object_or_404(Post, slug=slug)

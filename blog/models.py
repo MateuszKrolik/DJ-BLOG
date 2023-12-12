a253e340333262f52dcc80ff81f6e57dcaa2063a
Mateusz <mateuszkrolik87@gmail.com>
2023-12-12 18:28:03 +0100

    🐡 26th Commit
    Created Comment model with fields for username, email, comment text, and related post.
    Set up one-to-many relationship between Post and Comment models.
    Ran migrations to update database schema.
    epared for addition of comment form in post detail template.

--- blog/models.py
+++ blog/models.py
@@ -31,3 +31,11 @@ class Post(models.Model):
     author = models.ForeignKey(
         Author, on_delete=models.SET_NULL, null=True, related_name="posts")
     tags = models.ManyToManyField(Tag)
+
+
+class Comment(models.Model):
+    user_name = models.CharField(max_length=120)
+    user_email = models.EmailField()
+    text = models.TextField(max_length=400)
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")

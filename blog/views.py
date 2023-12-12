f49f3c3da01b5847badf376cf5eada2a7cd2e0f2
Mateusz <mateuszkrolik87@gmail.com>
2023-12-12 15:20:30 +0100

    🦇 21st Commit
    Updated all posts and post detail views to fetch data from the Post model.
    Removed unused code.
    Implemented ordering of posts by date.
    Fixed post-detail.html template to use correct field name for image.

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,6 @@
-from datetime import date
-
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Post
-
-all_posts = [
-    
-]
-
-
-def get_date(post):
-    return post['date']
 
 # Create your views here.
 
@@ -21,12 +11,13 @@ def starting_page(request):
     })
 
 def posts(request):
+    all_posts = Post.objects.all().order_by("-date")
     return render(request, "blog/all-posts.html", {
         "all_posts": all_posts
     })
 
 def post_detail(request, slug):
-    indentified_post = next(post for post in all_posts if post['slug'] == slug)
+    indentified_post = get_object_or_404(Post, slug=slug)
     return render(request, "blog/post-detail.html", {
         "post": indentified_post
     })

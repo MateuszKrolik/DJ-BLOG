37ce8d08eb68f6cbad6d92830ba641b45216c916
Mateusz <mateuszkrolik87@gmail.com>
2023-12-12 22:00:25 +0100

    ✨ 33rd Commit
    Enabled comment management in the admin area.
    Registered the Comment model in admin.py.
    Added a custom admin class to control the display of comments in the admin area.
    Updated the str method in the Post model to display the title when output as a string.

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Author, Post, Tag
+from .models import Author, Post, Tag, Comment
 
 # Register your models here.
 
@@ -9,6 +9,10 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ("title", "date", "author",)
     prepopulated_fields = {"slug": ("title",)}
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("user_name", "post")
+
 admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
 admin.site.register(Tag)
+admin.site.register(Comment, CommentAdmin)
--- blog/models.py
+++ blog/models.py
@@ -32,6 +32,9 @@ class Post(models.Model):
         Author, on_delete=models.SET_NULL, null=True, related_name="posts")
     tags = models.ManyToManyField(Tag)
 
+    def __str__(self):
+        return self.title
+
 
 class Comment(models.Model):
     user_name = models.CharField(max_length=120)

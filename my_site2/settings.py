f67a4475d388800cba60243faf3f88f4547cbfff
Mateusz <mateuszkrolik87@gmail.com>
2023-12-13 15:59:40 +0100

    🌍 40th Commit
    Configured static and media files settings for production.
    Set STATIC_ROOT to collect all static files into a single directory.
    Ensured separation of static and user-uploaded files for security reasons.

--- my_site2/settings.py
+++ my_site2/settings.py
@@ -118,17 +118,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "/static/"
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 MEDIA_ROOT = BASE_DIR / "uploads"
 MEDIA_URL = "/files/" #called it files to make it clear that these two can differ

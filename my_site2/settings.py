ff41ca39d9559f9d92357459bdff0d90db89ab7e
Mateusz <mateuszkrolik87@gmail.com>
2023-12-13 17:05:56 +0100

    🍎 43rd Commit
    Configured ALLOWED_HOSTS, SECRET_KEY, and DEBUG settings to use environment variables for better security and flexibility.
    This allows the application to adapt to different environments (development, production, etc.) by simply changing the environment variables.
    Also added a .env file in projects root directory that contains environment variables for development, which I load in settings and included it in my .gitignore file for security.

--- my_site2/settings.py
+++ my_site2/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from os import getenv
+from dotenv import load_dotenv
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +24,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-4us@&jd+l$unqgzg7$(si3j-pxzm7y*c+kiivn*lww!o4c&$#5"
+SECRET_KEY = getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = getenv("IS_DEVELOPMENT") 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    getenv("APP_HOST")
+]
 
 
 # Application definition

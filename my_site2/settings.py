fdbf9619333d7b5dfd8eb61e64a76fce8bf0a1d0
Mateusz <mateuszkrolik87@gmail.com>
2023-12-13 21:15:14 +0100

    🦍 48th Commit
    Add custom storage file in projects rood directory with classes for static and media files using AWS S3  and configured Django to use them.

--- my_site2/settings.py
+++ my_site2/settings.py
@@ -152,5 +152,8 @@ AWS_SECRET_ACCESS_KEY = getenv("AWS_SECRET_ACCESS_KEY")
 
 AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 
-STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+STATICFILES_FOLDER = "static"
+MEDIAFILES_FOLDER = "media"
+
+STATICFILES_STORAGE = "custom_storages.StaticFileStorage"
+DEFAULT_FILE_STORAGE = "custom_storages.MediaFileStorage"

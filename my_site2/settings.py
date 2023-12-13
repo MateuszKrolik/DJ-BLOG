1652da7aed68fafe71759fd4ba4a71f2b05e405b
Mateusz <mateuszkrolik87@gmail.com>
2023-12-13 18:39:57 +0100

    🍓 45th Commit
    Updated database configuration to use PostreSQL  and AWS RDS instead of SQLite, as well as added environment variables.
    Added psycopg2-binary to requirements via pip freeze in virtual environment.
    Re-deployed new zip file with updated settings and requirements, while excluding the SQLite file.
    Edited inbound-rules for Security Groups in elastic beanstalk to allow all traffic instead of only my local ip address, enabling new database server on freshly deployed site.

--- my_site2/settings.py
+++ my_site2/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = "my_site2.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": getenv("DB_USER"),
+        "PASSWORD": getenv("DB_PASSWORD"),
+        "HOST": getenv("DB_HOST"),
+        "PORT": getenv("DB_PORT"),
     }
 }
 
